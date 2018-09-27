99eca2f867babe6e9f81d3643618f9292fb7027a
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-27 04:59:22 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,10 @@
-# Write your code here.
+speakers = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  
+  
