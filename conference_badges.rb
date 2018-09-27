47d8c1ab3327be94a472c0260ec763763430e99a
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-27 05:03:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,7 +5,7 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(speakers)
   messages = array.map {|name| item * 3}
 
 end 
