7c513472835b1b79229855e2b0b5e1530a9038ba
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-27 05:05:26 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,3 +11,9 @@ def batch_badge_creator(speakers)
     end 
   end
   
+def assign_rooms(speakers)
+  speakers.each_with_index.map do |name, index|
+    "Hello, #{name}! You'll be assigned to room #{index+1}!"
+  end
+end
+
