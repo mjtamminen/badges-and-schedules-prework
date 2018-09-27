43274c5e81aac78661bf72dbcb5a4a35e51f7a95
tamminen.mikkoj <tamminen.mikkoj@gmail.com>
2018-09-27 05:06:31 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,12 +17,12 @@ def assign_rooms(speakers)
   end
 end
 
-def printer(attendees)
-  batch_badge_creator(attendees).each do |badge|
+def printer(speakers)
+  batch_badge_creator(speakers).each do |badge|
     puts badge
   end
 
-  assign_rooms(attendees).each do |assignment|
+  assign_rooms(speakers).each do |assignment|
     puts assignment
   end
 end
